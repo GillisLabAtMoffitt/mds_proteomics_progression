bbb36a2adba77958c66a486cb7f3c16a6ab75f7e
chrisleitzinger <christelleleitzinger@gmail.com>
2025-10-27 15:52:38 -0400

    Update data

--- R/02.data_wrangling.R
+++ R/02.data_wrangling.R
@@ -13,20 +13,28 @@ manifest <-
 clinical_data <-
   read_csv(paste0(path, 
                   "/RawData/MDSProteomics_CMMLClinicalData",
+                  "/MDSProteomics_CMML_Merged_Long_20251027.csv"))
+enrollment_data <-
+  read_csv(paste0(path, 
+                  "/RawData/MDSProteomics_CMMLClinicalData",
                   "/MDSProteomics_CMML_ClinicalData_20251021.csv"))
 
-plate1_read <-
+# plate1_read <-
+#   read.csv(paste0(path, 
+#                   "/RawData/MDSProteomics_OlinkData_20251017",
+#                   "/Plate1_data_intensity_nmlzd_Extended_NPX_20251017.csv"),
+#            sep=";")
+# plate2_read <-
+#   read.csv(paste0(path, 
+#                   "/RawData/MDSProteomics_OlinkData_20251017",
+#                   "/Plate2_data_intensity_nmlzd_Extended_NPX_20251017.csv"),
+#            sep=";")
+plate_read <-
   read.csv(paste0(path, 
-                  "/RawData/MDSProteomics_OlinkData_20251017",
-                  "/Plate1_data_intensity_nmlzd_Extended_NPX_20251017.csv"),
-           sep=";")
-plate2_read <-
-  read.csv(paste0(path, 
-                  "/RawData/MDSProteomics_OlinkData_20251017",
-                  "/Plate2_data_intensity_nmlzd_Extended_NPX_20251017.csv"),
-           sep=";")
+                  "/RawData/MDSProteomics_OlinkData_20251021",
+                  "/Gillis_3plates_Extended_NPX_2025-10-21.csv"))
 
-###################################################################### II ### Data wrangling
+###################################################################### II ### CMML data wrangling
 manifest <- manifest %>% 
   filter(disease_type == "CMML") %>% 
   select(SampleID = sample_name, WellID = plate_well_id, 
@@ -34,44 +42,54 @@ manifest <- manifest %>%
   mutate(collection_date = as.Date(collection_date)) %>% 
   distinct(study_id, collection_date, .keep_all = TRUE)
 
+enrollment_data <- enrollment_data %>% 
+  select(SampleID, mrn, enrolldate) %>% 
+  mutate(mrn = as.character(mrn)) %>% 
+  distinct()
+
 clinical_data <- clinical_data %>% 
-  select(SampleID, mrn, collection_date,
-         overallpd : death_status) %>% 
+  filter(str_detect(SampleID, "CMML_")) %>% 
+  select(SampleID, MRN, collection_date,
+         PD_overall : AIE) %>% 
   distinct() %>% 
   mutate(collection_date = case_when(
     !is.na(collection_date)             ~ collection_date,
     is.na(collection_date)              ~ as.Date("2024-07-10")
   )) %>% 
-  mutate(mrn = as.character(mrn))
+  mutate(MRN = as.character(MRN))
 
-plate_read <- 
-  bind_rows(plate1_read, plate2_read, .id = "plate") %>%
+plate_read <- plate_read %>% 
+  # bind_rows(plate1_read, plate2_read, .id = "PlateID") %>%
   filter(str_detect(SampleID, "CMML_")) %>% 
   filter(AssayQC == "PASS") %>% 
-  select(SampleID, plate, WellID, PCNormalizedNPX, Assay)
+  select(SampleID, PlateID, WellID, PCNormalizedNPX, Assay)
 # a <- plate_read %>% distinct(SampleID, Assay)
-rm(plate1_read, plate2_read)
+# rm(plate1_read, plate2_read)
 
 wide_reads <- manifest %>% 
   # Use inner join to filter out the duplicate samples
   inner_join(., clinical_data, 
              by = c("SampleID", "collection_date")) %>% 
-  # distinct(SampleID, WellID, plate)
-  arrange(mrn, collection_date) %>% 
-  select(mrn, collection_date, everything()) %>% 
-  # distinct(mrn, SampleID, collection_date) %>% 
-  group_by(mrn) %>% 
-  mutate(sample_seq_num = row_number(), .after = mrn) %>% 
+  inner_join(., enrollment_data, 
+             by = c("SampleID", "MRN" = "mrn")) %>% 
+  # distinct(SampleID, WellID, PlateID)
+  arrange(MRN, collection_date) %>% 
+  select(MRN, collection_date, everything()) %>% 
+  # distinct(MRN, SampleID, collection_date) %>% 
+  group_by(MRN) %>% 
+  mutate(sample_seq_num = row_number(), .after = MRN) %>% 
   mutate(sample_seq_num = paste0("date", sample_seq_num)) %>% 
   ungroup() %>% 
+  mutate(PlateID = paste0("GillisPadron_plate", plate)) %>% 
+  select(-plate) %>% 
   inner_join(., plate_read,
-             by = c("SampleID", "WellID", "plate")) %>% 
-  select(-c(SampleID, WellID, plate, sample_id))
+             by = c("SampleID", "WellID", "PlateID")) %>% 
+  select(-c(SampleID, WellID, PlateID, sample_id))
 
 
 
 wide_reads |>
-  dplyr::summarise(n = dplyr::n(), .by = c(mrn, collection_date, Assay)) |>
+  dplyr::summarise(n = dplyr::n(), .by = c(MRN, collection_date, Assay)) |>
   dplyr::filter(n > 1L) 
 
 colnames(wide_reads)
@@ -84,34 +102,55 @@ wide_cmml_data <- wide_reads %>%
   rename(collection = collection_date) %>% 
   pivot_wider(#id_cols = mrn, 
               names_from = sample_seq_num, 
-              values_from = -c(mrn, sample_seq_num, study_id, overallpd, overallpddate,
-                               enrolldate, os, acuteevent, death, death_status),
+              values_from = -c(MRN, sample_seq_num, study_id, PD_overall, PD_date,
+                               enrolldate,
+                               os, AIE, # AIE = acuteevent, 
+                               # death, 
+                               death_status),
               names_vary = "slowest")
 
+write_csv(wide_cmml_data,
+          paste0("data/processed_data",
+                 "/MDSProteomics_wide_cmml_rawdata_",
+                 str_remove_all(today(), "-"), ".csv"))
+
+write_csv(wide_cmml_data,
+          paste0(path, "/ProcessedData",
+                 "/MDSProteomics_wide_cmml_rawdata_",
+                 str_remove_all(today(), "-"), ".csv"))
+
+
+###################################################################### III ### Add and rename variables
+wide_cmml_data <- wide_cmml_data %>% 
+  rename(vital_status = death_status,
+         os_date = os) %>% 
+  mutate(os_event = case_when(
+    vital_status == "DEAD"                     ~ 1,
+    vital_status == "ALIVE"                    ~ 0
+  ), .after = vital_status) %>% 
+  mutate(os_time_from_enroll_months = interval(start = enrolldate,
+                                           end = os_date)/
+           duration(n = 1, unit = "months"), .after = os_date) %>%
+  mutate(pfs_event = case_when(
+    PD_overall == 1                            ~ 1,
+    vital_status == "DEAD"                     ~ 1,
+    vital_status == "ALIVE"                    ~ 0
+  ), .after = PD_date) %>% 
+  mutate(pfs_date = coalesce(PD_date, os_date), .after = pfs_event) %>% 
+  mutate(pfs_time_from_enroll_months = interval(start = enrolldate,
+                                                  end = pfs_date)/
+           duration(n = 1, unit = "months"), .after = pfs_date)
 
 write_csv(wide_cmml_data, 
           paste0("data/processed_data",
-                 "/MDSProteomics_wide_cmml_data_", 
+                 "/MDSProteomics_cmml_analysisdata_", 
                  str_remove_all(today(), "-"), ".csv"))
 
 write_csv(wide_cmml_data, 
           paste0(path, "/ProcessedData",
-                 "/MDSProteomics_wide_cmml_data_", 
+                 "/MDSProteomics_cmml_analysisdata_", 
                  str_remove_all(today(), "-"), ".csv"))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# END CMML----
 
